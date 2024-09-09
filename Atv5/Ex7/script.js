26f44cf2edc2bd3327101955d036e211d4df8a2f
Marangon <marangaluucas13@gmail.com>
2024-09-09 03:05:21 -0300

    Update 1.00

---
+++ Atv5/Ex7/script.js
@@ -0,0 +1,11 @@
+function calcularDobro(event) {
+    if (event.key === "Enter") {
+        let lado = parseFloat(document.getElementById("lado").value);
+        
+        let area = Math.pow(lado, 2);
+        let dobroDaArea = area * 2;
+        
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = `A área do quadrado é ${area.toFixed(2)} metros quadrados. O dobro da área é ${dobroDaArea.toFixed(2)} metros quadrados.`;
+    }
+}
